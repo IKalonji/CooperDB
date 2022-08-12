beb9177b816e424dae437af1b3e5e10dea1d43f8
mothupir <mothupir@bbd.co.za>
2022-08-12 10:26:57 +0200

    .

---
+++ test.py
@@ -0,0 +1,21 @@
+from CooperQL_parser import *
+
+def main(): 
+    db = parse_input({
+        "query": "create_database",
+        "database_name": "mydb"
+    })
+
+    db2 = parse_input({
+        "query": "create_database",
+        "database_name": "mydb2"
+    })
+
+    """tbl = parse_input({
+        "query": ""
+    })"""
+
+    print(db.database);
+
+
+main()
